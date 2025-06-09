f351d4896026edf03564e43b0eb6bc2714917aa1
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-06-09 16:34:48 -0500

    first commit

---
+++ src/test/java/com/cinemaproject/appcore/AppcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaproject.appcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
